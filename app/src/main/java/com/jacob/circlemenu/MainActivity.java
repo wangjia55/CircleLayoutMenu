0ba6597fc4e408270a677409ab1a4c3145a2711c
jacob-wj <wangjia199055@126.com>
2015-04-04 11:42:50 +0800

    重新整理了UI

--- app/src/main/java/com/jacob/circlemenu/MainActivity.java
+++ app/src/main/java/com/jacob/circlemenu/MainActivity.java
@@ -7,12 +7,12 @@ import android.support.v7.app.ActionBarActivity;
 public class MainActivity extends ActionBarActivity {
     //菜单按钮的资源图片
     private int[] imgs = new int[]{
-            R.mipmap.ic_item_1,
-            R.mipmap.ic_item_2,
-            R.mipmap.ic_item_3,
-            R.mipmap.ic_item_4,
-            R.mipmap.ic_item_5,
-            R.mipmap.ic_item_6};
+            R.drawable.ic_item_1,
+            R.drawable.ic_item_2,
+            R.drawable.ic_item_3,
+            R.drawable.ic_item_4,
+            R.drawable.ic_item_5,
+            R.drawable.ic_item_6};
 
     //菜单按钮对应的文案
     private int[] titles = new int[]{
@@ -28,10 +28,8 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        CircleMenu circleMenu = (CircleMenu) findViewById(R.id.circle_menu);
-        circleMenu.updateView();
-
-
+        CircleMenuLayout circleMenu = (CircleMenuLayout) findViewById(R.id.circle_menu);
+        circleMenu.setMenuResource(imgs,titles);
     }
 
 }
